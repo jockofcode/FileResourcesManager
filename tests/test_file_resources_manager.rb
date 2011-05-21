d5cda5d83a7dc149a5f79fdcb122cb2ca5a69ede
JockOfCode <branden@carbondatacomputers.com>
2011-05-20 18:24:27 -0600

    fixed tests after project wide rename...

--- tests/test_file_resources_manager.rb
+++ tests/test_file_resources_manager.rb
@@ -1,28 +1,28 @@
-require 'settings_manager'
+require 'file_resources_manager'
 require 'test/unit'
 
-class TestSettingsManager < Test::Unit::TestCase
+class TestFileResourcesManager < Test::Unit::TestCase
 	def test_get
-		assert_nil(SettingsManager::get("some_non_existing_settings"),"There is none so it should most definately fail")
+		assert_nil(FileResourcesManager::get("some_non_existing_file_resources"),"There is none so it should most definately fail")
 	end
-	def test_system_settings
-		test_directory_location = File.join(Dir.pwd,"settings_location_test")
+	def test_system_file_resources
+		test_directory_location = File.join(Dir.pwd,"file_resources_location_test")
 		if File.directory?(test_directory_location) 
 			(Dir.entries(test_directory_location) - [".",".."]).each{|f| File.delete(File.join(test_directory_location,f)) }
 			Dir.rmdir(test_directory_location) 
 		end
 		ENV.delete "APP_SETTING"
-		assert_equal(SettingsManager::main_settings_file_location,File.expand_path("~/.app_settings"),"if none exist then this should be $HOME/.app_settings")
+		assert_equal(FileResourcesManager::main_file_resources_file_location,File.expand_path("~/.app_file_resources"),"if none exist then this should be $HOME/.app_file_resources")
 		ENV["APP_SETTING"] = test_directory_location 
-		assert_equal(SettingsManager::main_settings_file_location,test_directory_location,"Main Setting File Location Should be set to the current directory + settings_location_test")
-		SettingsManager::ensure_main_settings_file_exists
-		assert_equal(File.exists?(File.join(test_directory_location,".main_settings.yaml")),true,"settings file should have been created")
-		assert_equal(SettingsManager::get_settings_location("test_kv"),File.join(test_directory_location,"test_kv.yml"),"test settings file location should match")
-		SettingsManager::set("test_kv",{:name => "jockofcode",:occupation => "rubyist" })
-		assert_equal(SettingsManager::get("test_kv")[:name],"jockofcode","should have retreived 'jockofcode' from settings")
-		SettingsManager::set_settings_location("test_kv",File.join(test_directory_location,"not_the_default_kv.yml"))
-		assert_nil(SettingsManager::get("test_kv"),"It should not be getting the value from test_kv.yml anymore")
-		SettingsManager::set("test_kv",{:name => "john",:occupation => "kneeder"})
-		assert_equal(SettingsManager::get("test_kv")[:name],"john","should now be john, not jockofcode")
+		assert_equal(FileResourcesManager::main_file_resources_file_location,test_directory_location,"Main Setting File Location Should be set to the current directory + file_resources_location_test")
+		FileResourcesManager::ensure_main_file_resources_file_exists
+		assert_equal(File.exists?(File.join(test_directory_location,".main_file_resources.yaml")),true,"file_resources file should have been created")
+		assert_equal(FileResourcesManager::get_file_resources_location("test_kv"),File.join(test_directory_location,"test_kv.yml"),"test file_resources file location should match")
+		FileResourcesManager::set("test_kv",{:name => "jockofcode",:occupation => "rubyist" })
+		assert_equal(FileResourcesManager::get("test_kv")[:name],"jockofcode","should have retreived 'jockofcode' from file_resources")
+		FileResourcesManager::set_file_resources_location("test_kv",File.join(test_directory_location,"not_the_default_kv.yml"))
+		assert_nil(FileResourcesManager::get("test_kv"),"It should not be getting the value from test_kv.yml anymore")
+		FileResourcesManager::set("test_kv",{:name => "john",:occupation => "kneeder"})
+		assert_equal(FileResourcesManager::get("test_kv")[:name],"john","should now be john, not jockofcode")
 	end
 end
